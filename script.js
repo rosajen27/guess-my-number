3b17c34e5bfa792039d679e95ac2b9253b749435
Jennifer Rosa <rosajen27@gmail.com>
2021-01-21 19:22:56 -0500

    add event listener to check button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// save value in the guess box when the check button is clicked
+document.querySelector(".check").addEventListener("click", function () {
+    
+    // Number() turns string into an integer
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess, typeof guess);
+
+    // if guess field is empty, no number message appears
+    if (!guess) {
+        document.querySelector(".message").textContent = "⛔ No Number!";
+    }
+});
