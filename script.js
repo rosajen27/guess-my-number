5a4644a877565d813a1b5bde348a04240adfb85a
Jennifer Rosa <rosajen27@gmail.com>
2021-01-22 19:46:50 -0500

    decrease score if guess is incorrect

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // generate a random secretNumber between 1 - 20
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
+// starting score is 20
+let score = 20;
+
 // display that random secretNumber in the secretNumber div
 document.querySelector(".number").textContent = secretNumber;
 
@@ -14,15 +17,24 @@ document.querySelector(".check").addEventListener("click", function () {
     console.log(guess, typeof guess);
 
     // if guess field is empty, no number message appears
-    // if guess equals secretNumber, display correct number
+    // if guess equals secretNumber, display correct number message
     // if guess is too low or too high, display corresponding message
     if (!guess) {
         document.querySelector(".message").textContent = "⛔ No Number!";
+
     } else if (guess === secretNumber) {
         document.querySelector(".message").textContent = "🎉 Correct Number!";
+
     } else if (guess > secretNumber) {
         document.querySelector(".message").textContent = "📈 Too high!"
+        // reduce score by one point and display new score
+        score--;
+        document.querySelector(".score").textContent = score;
+
     } else if (guess < secretNumber) {
         document.querySelector(".message").textContent = "📉 Too low!"
+        // reduce score by one point and display new score
+        score--;
+        document.querySelector(".score").textContent = score;
     }
 });
